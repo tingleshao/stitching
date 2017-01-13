40a2f85da2f3c956292a0979af0793cc63b5f0cc
Chong Shao <tingleshao@gmail.com>
2017-01-13 14:05:00 -0500

    save update

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import cv2
 
-def stitch_images():
+def stitch_images(images, ratio =0.75, reprojThresh=4.0, showMatches=False):
     print "stitching..."
+    (imageB, imageA) = images
+    (kpsA, featuresA) = detect_and_describe(imageA)
+    (kpsB, featuresB) = detect_and_describe(imageB)
+    M = matchKeyPoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh)
+    if M is None:
+        return None
+    (matches, H, status) = M
+    result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+    result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+    if showMatches:
+        vis = drawMatches(imageA, imageB, kpsA, kpsB, matches, status)
+        return (result, vis)
+    return result
+
+
+def detect_and_describe(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    descriptor = cv2.xfeatures2d.SIFT_create()
+    (kps, features) = descriptor.detectAndCompute(image, None)
+
+    kps = np.float32([kp.pt for kp in kps])
+    return (kps, features)    
+
 
 
 def main():
